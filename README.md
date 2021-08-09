a552644371fc1f2bf8d8e01821f3f7b77f1bd935
yukukky <kukita.yusuke@g.wakayama-u.jp>
2021-08-09 20:20:04 +0900

    send slack incoming webhook

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # backlog-to-slack-notification
+## About
+Notification script from backlog to slack
+Convert backlog mentions to slack mentions
+
+Backlogの通知をSlackへ送る
+BacklogのメンションをSlackのメンションへ変換
+## For User(EN)
+1. Add Incoming Webhook at Slack Channel.
+1. Change mention_list.json.
+    1. add Nulab unique ID. (see https://apps.nulab.com/profile/account)
+    1. add Slack Member ID.
+1. deploy GCP Cloud Function.
+    1. set trigger type `HTTP`.
+    1. Set the environment variable `SLACK_WEBHOOK_URL` to the Incoming Webhook URL.
+    1. deploy main.py, mention_list.json, requirements.txt
+1. Set Backlog Workspace Webhook.
+    1. set GCP Cloud Functions URL.
+      (https://support.backlog.com/hc/en-us/articles/115015420967-Webhooks)
+
+## For User(JP)
+1. Incoming WebhookをSlackに追加し、Webhook URLを取得
+1. mention_list.jsonを書き換える
+    1. NulabユニークIDを追加 https://apps.nulab.com/profile/account
+    1. Slack Member IDを追加
+1. GCPのCloud Functionsにデプロイ（Python3.9）
+    1. トリガーのタイプはHTTP
+    1. 環境変数 `SLACK_WEBHOOK_URL` にIncoming Webhook URLをセット
+    1. main.py, mention_list.json, requirements.txtをデプロイ
+1. プロジェクト設定＞インテグレーション＞Webhookを設定
+    1. WebHook URLにCloud Functionsで発行されたURLをセット
+      (https://support-ja.backlog.com/hc/ja/articles/360036147713-Webhook)
+
+
+
+## For Developer
+1. install pipenv
+2. exec 'pipen install --dev'
--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+import json
+from slack_sdk.webhook import WebhookClient
+from flask import make_response
+
+
 def index(request):
     """Responds to any HTTP request.
     Args:
@@ -8,15 +14,30 @@ def index(request):
         `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
     """
     request_json = request.get_json()
-    print(request_json)
-
-    request_content = request_json["content"] if "content" in request_json else None
-    summary = request_content["summary"] if "summary" in request_content else None
     request_notifications = (
         request_json["notifications"] if "notifications" in request_json else None
     )
-    print(summary)
-    for notification in request_notifications:
-        print(notification)
+    project_name = request_json["project"]["name"]
+    project_key = request_json["project"]["projectKey"]
+    content_key_id = request_json["content"]["key_id"]
+    url = f"https://{project_name}.backlog.com/view/{project_key}-{content_key_id}"
+    mentionList = getMentionList(request_notifications)
+    message = " ".join(mentionList) + " Notification from " + url
 
-    return "OK"
+    webhook_url = os.environ["SLACK_WEBHOOK_URL"]
+    webhook = WebhookClient(webhook_url)
+    webhook.send(text=message)
+
+    return make_response("", 200)
+
+
+def getMentionList(request_notifications):
+    json_open = open("mention_list.json", "r")
+    json_load = json.load(json_open)
+    mentionList = list()
+    for notification in request_notifications:
+        if notification["user"]["nulabAccount"]["uniqueId"]:
+            uniqueId = notification["user"]["nulabAccount"]["uniqueId"]
+            if uniqueId in json_load:
+                mentionList.append(f"<@{json_load[uniqueId]}>")
+    return mentionList
